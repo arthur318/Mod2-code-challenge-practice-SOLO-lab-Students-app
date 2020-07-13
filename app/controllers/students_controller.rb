378b6d2bc3c311430ee83f5802bb16a17e2146a8
arthur318 <arthurhwang318@gmail.com>
2020-07-12 21:22:43 -0700

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,32 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
   
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+    @instructors = Instructor.all
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.valid?
+      @student.save
+      redirect_to @student.instructor
+    else
+      flash[:errors] = @student.errors.full_messages
+      redirect_to "/students/new"
+    end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :major, :age, :instructor_id)
+  end
+
+
+
 end
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,12 @@
 class Instructor < ApplicationRecord
     has_many :students
+
+    def avg_age
+        self.students.sum(&:age)/self.students.length
+    end
+
+    def student_sorted
+        # self.students.sort_by(&:name)
+        self.students.sort_by { |student| student.name }
+    end
 end
